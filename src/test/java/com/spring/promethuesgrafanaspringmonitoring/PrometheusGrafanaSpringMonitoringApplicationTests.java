4df66bc4c24006afb33fcf2f2d911b2f9683bc81
talhashan <talha.shan007@gmail.com>
2023-04-30 14:46:30 +0500

    Logback event implementation & visualization

---
+++ src/test/java/com/spring/promethuesgrafanaspringmonitoring/PrometheusGrafanaSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.promethuesgrafanaspringmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusGrafanaSpringMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
